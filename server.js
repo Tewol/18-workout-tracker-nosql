afacdd230e18c49ff5d85fda96757cb053cb3898
Hewan Redie <tewol22@gmail.com>
2021-08-05 22:44:23 -0500

    deploying

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3000
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
-// routes
-app.use(require("./public/api.js"));
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+//require("./routes/api")(app);
+//require("./routes/html")(app);
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
